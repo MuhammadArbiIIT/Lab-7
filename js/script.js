a50254aff17bfddb6c7fe10e15614b597c07e54b
MuhammadArbiIIT <marbi@hawk.iit.edu>
2022-04-05 16:53:24 -0500

    added an event listeneer and a button on the alert me div

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 function init(){
     //add your javascript between these two lines of code
-     
-    
-    
-    
-    
-    
-    
-    
+    function alertFunction(){
+        var text = document.getElementById("entryinput").value;
+        var text2 = document.getElementById("textoutput");
+        alert("Muhammad Arbi: " + text);
+        document.getElementById("textoutput").innerHTML = text
+    }
+    var button = document.getElementById('entrybutton');
+    button.addEventListener('click', alertFunction);
+}
+
     window.addEventListener('load', init);
